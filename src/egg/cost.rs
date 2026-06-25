e4a94a17e7527958a45ae7a0f2eefa364796c897
ashiven <nevisha@pm.me>
2026-06-25 14:46:39 +0200

    remove dummy cost impl

--- src/egg/cost.rs
+++ src/egg/cost.rs
@@ -4,15 +4,3 @@ use egg::*;
 
 // AUTOGEN START: egg-cost-rust-impl
 // AUTOGEN END: egg-cost-rust-impl
-
-#[derive(Debug)]
-pub struct MyCost;
-impl CostFunction<Mim> for MyCost {
-    type Cost = usize;
-    fn cost<C>(&mut self, enode: &Mim, mut costs: C) -> Self::Cost
-    where
-        C: FnMut(Id) -> Self::Cost,
-    {
-        enode.fold(1, |sum, id| sum.saturating_add(costs(id)))
-    }
-}
